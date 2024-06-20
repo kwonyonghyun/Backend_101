a049eedbc83e21ecd6e927d2072760e06108c1be
kwonyonghyun <rnjs990523@naver.com>
2024-06-20 14:01:37 +0900

    Initial commit

---
+++ src/main/java/com/example/Backend_101/Backend101Application.java
@@ -0,0 +1,13 @@
+package com.example.Backend_101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Backend_101/Backend101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
